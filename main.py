10e8c42df5429aa1dd7540493f8dd411675411e6
Mateusz <matiutd888@gmail.com>
2023-12-14 03:35:50 +0100

    Add examples notebook

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 import networkx as nx
 import matplotlib.pyplot as plt
+from queue import Queue
+import sys
 
 
-def
+def algorithm_unoptimized_buid(graph, starting_nodes, _final_vertices):
+    visited = {s_i: [] for s_i in starting_nodes}
+    open = Queue()
+    for s in starting_nodes:
+        open.put(s)
 
-def hello_world():
+    while not open.empty():
+        current_node = open.get()
+        for n in graph.neighbors(current_node):
+            if n not in visited:
+                visited[n] = [current_node]
+            else:
+                parents = visited[n]
+                parents.append(current_node)
+                visited[n] = parents
+    return visited
+
+
+def dict_to_networkx_graph(graph_dict):
+    G = nx.Graph()
+    for node, neighbors in graph_dict.items():
+        G.add_node(node)
+        G.add_edges_from((node, neighbor) for neighbor in neighbors)
+
+    return G
+
+
+def handle_graph_unoptimized(graph, starting_nodes, final_vertices):
+    g = algorithm_unoptimized_buid(graph, starting_nodes, final_vertices)
+    nx_g = dict_to_networkx_graph(g)
+    cycles = list(nx.simple_cycles(nx_g))
+    if len(cycles) > 0:
+        for cycle in cycles:
+            print(cycle, file=sys.stderr)
+        return False
+    else:
+        return True
+
+
+def get_random_graph_with_starting_vertices(n=10, p=0.3, n_starting_vertices=2, n_final_vertices=2, seed=None):
     # Generate a random graph with 10 nodes
-    G = nx.gnp_random_graph(10, p=0.3)
+    g = nx.gnp_random_graph(n, p, directed=True, seed=seed)
+    nodes = list(g.nodes())
+    starting_vertices = nodes[:n_starting_vertices].copy()
+    final_vertices = nodes[n_starting_vertices:(n_starting_vertices + n_final_vertices)].copy()
+    return g, starting_vertices, final_vertices
 
-    # Plot the graph
-    nx.draw(G, with_labels=True, font_weight='bold')
-    plt.show()
+def simulate(n_simulations=100, initial_seed=42):
+    seed = initial_seed
+    for i in range(n_simulations):
+        g, starting, final = get_random_graph_with_starting_vertices(seed=seed)
+        if not handle_graph_unoptimized(g, starting, final):
+            sys.exit(1)
+        seed += 1
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    hello_world()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    simulate(100, 42)
